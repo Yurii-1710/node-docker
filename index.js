328a13d9a6eef4060f574490fcd28dc7f1f55aed
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-08-24 20:55:04 +0200

    Added CRUD + auth

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require("express")
 const mongoose = require('mongoose');
 
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
+
 const app = express()
 
-mongoose.connect(
-"mongodb://admin:password@db:27017/?authSource=admin"
-).then(() => console.log('Connected to DB'))
-.catch((e) => console.log(e))
+const mongo_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const postRouter = require("./routes/postRoutes")
+const userRouter = require("./routes/userRoutes")
+
+const connectWithRetry = () => {
+  mongoose
+  .connect(mongo_URL)
+  .then(() => console.log('Connected to DB'))
+  .catch((e) => {
+    console.log(e)
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
+connectWithRetry()
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("<h2>Fuck you!</h2>")
+  res.send("<h2>Fuck you</h2>")
 })
 
+app.use("/api/v1/posts", postRouter)
+app.use("/api/v1/users", userRouter)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
